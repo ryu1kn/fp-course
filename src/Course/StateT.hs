3c2e88910b21899baa57c5852e840b0867603009
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-12 22:01:56 +1000

    WIP StateT

--- src/Course/StateT.hs
+++ src/Course/StateT.hs
@@ -39,8 +39,7 @@ instance Functor k => Functor (StateT s k) where
     (a -> b)
     -> StateT s k a
     -> StateT s k b
-  (<$>) =
-    error "todo: Course.StateT (<$>)#instance (StateT s k)"
+  f <$> StateT ma = StateT $ (<$>) (first f) . ma
 
 -- | Implement the `Applicative` instance for @StateT s k@ given a @Monad k@.
 --
@@ -62,8 +61,8 @@ instance Monad k => Applicative (StateT s k) where
   pure ::
     a
     -> StateT s k a
-  pure =
-    error "todo: Course.StateT pure#instance (StateT s k)"
+  pure a = StateT $ \s -> pure (a, s)
+
   (<*>) ::
     StateT s k (a -> b)
     -> StateT s k a
